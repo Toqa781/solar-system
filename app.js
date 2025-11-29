5cdc4ce59498c9f212673c8fbb02a65f8e1b6225
Toqa Abdalla <132062106+Toqa781@users.noreply.github.com>
2025-11-29 19:38:24 +0200

    remove healthchecks

--- app.js
+++ app.js
@@ -78,10 +78,6 @@ app.get('/ready',   function(req, res) {
         "status": "ready"
     });
 })
-// Health check endpoint
-app.get('/health', (req, res) => {
-  // Check MongoDB connection
-  const dbStatus = mongoose.connection.readyState === 1 ? 'connected' : 'disconnected';
   
   if (dbStatus === 'connected') {
     res.status(200).json({
